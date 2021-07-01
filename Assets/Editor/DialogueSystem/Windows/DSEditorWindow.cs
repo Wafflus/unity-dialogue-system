71b0af965480854a442430887c5aecee080bedc7
Wafflus <wafflusdev@gmail.com>
2021-07-01 12:59:12 +0100

    Add editor window script

---
+++ Assets/Editor/DialogueSystem/Windows/DSEditorWindow.cs
@@ -0,0 +1,13 @@
+using UnityEditor;
+
+namespace DS.Windows
+{
+    public class DSEditorWindow : EditorWindow
+    {
+        [MenuItem("Window/DS/Dialogue Graph")]
+        public static void Open()
+        {
+            GetWindow<DSEditorWindow>("Dialogue Graph");
+        }
+    }
+}
