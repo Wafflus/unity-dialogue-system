4fb7bcdfba852883dbe0a9be15c7eef21be767cd
Wafflus <wafflusdev@gmail.com>
2021-08-30 23:43:45 +0100

    Add node error data script

---
+++ Assets/Editor/DialogueSystem/Data/Error/DSNodeErrorData.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace DS.Data.Error
+{
+    using Elements;
+
+    public class DSNodeErrorData
+    {
+        public DSErrorData ErrorData { get; set; }
+        public List<DSNode> Nodes { get; set; }
+
+        public DSNodeErrorData()
+        {
+            ErrorData = new DSErrorData();
+            Nodes = new List<DSNode>();
+        }
+    }
+}
