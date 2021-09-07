dfee805081a4100592cc9e51e6572580b773f836
Wafflus <wafflusdev@gmail.com>
2021-09-07 17:26:28 +0100

    Add node save data editor script

---
+++ Assets/Editor/DialogueSystem/Data/Save/DSNodeSaveData.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DS.Data.Save
+{
+    using Enumerations;
+
+    [Serializable]
+    public class DSNodeSaveData
+    {
+        [field: SerializeField] public string Name { get; set; }
+        [field: SerializeField] public string Text { get; set; }
+        [field: SerializeField] public List<string> Choices { get; set; }
+        [field: SerializeField] public string GroupID { get; set; }
+        [field: SerializeField] public DSDialogueType DialogueType { get; set; }
+        [field: SerializeField] public Vector2 Position { get; set; }
+    }
+}
