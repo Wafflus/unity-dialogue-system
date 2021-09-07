027279f93c02639456a84cbbada3c7433ebec171
Wafflus <wafflusdev@gmail.com>
2021-09-07 17:44:15 +0100

    Add choice save data editor script

---
+++ Assets/Editor/DialogueSystem/Data/Save/DSChoiceSaveData.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+namespace DS.Data.Save
+{
+    [Serializable]
+    public class DSChoiceSaveData
+    {
+        [field: SerializeField] public string Text { get; set; }
+        [field: SerializeField] public string NodeID { get; set; }
+    }
+}
