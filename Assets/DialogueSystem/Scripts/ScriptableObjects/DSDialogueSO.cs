72a767487487f135189f800367614fde037a1cf1
Wafflus <wafflusdev@gmail.com>
2021-09-08 11:48:13 +0100

    Add dialogue scriptable object script

---
+++ Assets/DialogueSystem/Scripts/ScriptableObjects/DSDialogueSO.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DS.ScriptableObjects
+{
+    using Enumerations;
+
+    public class DSDialogueSO : ScriptableObject
+    {
+        [field: SerializeField] public string DialogueName { get; set; }
+        [field: SerializeField] [field: TextArea()] public string Text { get; set; }
+        [field: SerializeField] public List<string> Choices { get; set; }
+        [field: SerializeField] public DSDialogueType DialogueType { get; set; }
+        [field: SerializeField] public bool IsStartingDialogue { get; set; }
+
+        public void Initialize(string dialogueName, string text, List<string> choices, DSDialogueType dialogueType, bool isStartingDialogue)
+        {
+            DialogueName = dialogueName;
+            Text = text;
+            Choices = choices;
+            DialogueType = dialogueType;
+            IsStartingDialogue = isStartingDialogue;
+        }
+    }
+}
