7438e0792efdf80dff524a2e0a1f8a4434b35c45
Wafflus <wafflusdev@gmail.com>
2021-09-15 19:23:25 +0100

    Create dialogue system IO Utility GetElementsFromGraphView method

--- Assets/Editor/DialogueSystem/Utilities/DSIOUtility.cs
+++ Assets/Editor/DialogueSystem/Utilities/DSIOUtility.cs
@@ -1,21 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEditor;
+using UnityEngine;
 
 namespace DS.Utilities
 {
+    using Elements;
+    using Windows;
+
     public static class DSIOUtility
     {
+        private static DSGraphView graphView;
+
         private static string graphFileName;
         private static string containerFolderPath;
 
-        public static void Initialize(string graphName)
+        private static List<DSNode> nodes;
+        private static List<DSGroup> groups;
+
+        public static void Initialize(DSGraphView dsGraphView, string graphName)
         {
+            graphView = dsGraphView;
+
             graphFileName = graphName;
             containerFolderPath = $"Assets/DialogueSystem/Dialogues/{graphName}";
+
+            nodes = new List<DSNode>();
+            groups = new List<DSGroup>();
         }
 
         public static void Save()
         {
             CreateDefaultFolders();
+
+            GetElementsFromGraphView();
         }
 
         private static void CreateDefaultFolders()
@@ -29,6 +48,30 @@ namespace DS.Utilities
             CreateFolder(containerFolderPath, "Global");
             CreateFolder(containerFolderPath, "Groups");
             CreateFolder($"{containerFolderPath}/Global", "Dialogues");
+        }
+
+        private static void GetElementsFromGraphView()
+        {
+            Type groupType = typeof(DSGroup);
+
+            graphView.graphElements.ForEach(graphElement =>
+            {
+                if (graphElement is DSNode node)
+                {
+                    nodes.Add(node);
+
+                    return;
+                }
+
+                if (graphElement.GetType() == groupType)
+                {
+                    DSGroup group = (DSGroup) graphElement;
+
+                    groups.Add(group);
+
+                    return;
+                }
+            });
         }
 
         private static void CreateFolder(string parentFolderPath, string newFolderName)
