5ec3a2d670037c37122e0ce640d9a51a188fd5c3
Wafflus <wafflusdev@gmail.com>
2021-09-15 14:29:54 +0100

    Create dialogue system IO Utility CreateFolder method

--- Assets/Editor/DialogueSystem/Utilities/DSIOUtility.cs
+++ Assets/Editor/DialogueSystem/Utilities/DSIOUtility.cs
@@ -1,9 +1,21 @@
+using UnityEditor;
+
 namespace DS.Utilities
 {
     public static class DSIOUtility
     {
         public static void Save()
         {
+        }
+
+        private static void CreateFolder(string parentFolderPath, string newFolderName)
+        {
+            if (AssetDatabase.IsValidFolder($"{parentFolderPath}/{newFolderName}"))
+            {
+                return;
+            }
+
+            AssetDatabase.CreateFolder(parentFolderPath, newFolderName);
         }
     }
 }
