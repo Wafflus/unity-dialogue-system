81dc17f4e6b0b3f6f251dd83281a5d1904864800
Wafflus <wafflusdev@gmail.com>
2021-09-15 14:31:15 +0100

    Add default folders creation to IO Utility Save method

--- Assets/Editor/DialogueSystem/Utilities/DSIOUtility.cs
+++ Assets/Editor/DialogueSystem/Utilities/DSIOUtility.cs
@@ -4,8 +4,31 @@ namespace DS.Utilities
 {
     public static class DSIOUtility
     {
+        private static string graphFileName;
+        private static string containerFolderPath;
+
+        public static void Initialize(string graphName)
+        {
+            graphFileName = graphName;
+            containerFolderPath = $"Assets/DialogueSystem/Dialogues/{graphName}";
+        }
+
         public static void Save()
         {
+            CreateDefaultFolders();
+        }
+
+        private static void CreateDefaultFolders()
+        {
+            CreateFolder("Assets/Editor/DialogueSystem", "Graphs");
+
+            CreateFolder("Assets", "DialogueSystem");
+            CreateFolder("Assets/DialogueSystem", "Dialogues");
+
+            CreateFolder("Assets/DialogueSystem/Dialogues", graphFileName);
+            CreateFolder(containerFolderPath, "Global");
+            CreateFolder(containerFolderPath, "Groups");
+            CreateFolder($"{containerFolderPath}/Global", "Dialogues");
         }
 
         private static void CreateFolder(string parentFolderPath, string newFolderName)
