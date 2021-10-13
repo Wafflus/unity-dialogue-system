abd06528582bd47939ca9e3a2848efe7b0846095
Wafflus <wafflusdev@gmail.com>
2021-10-13 17:45:42 +0100

    Create dialogue container scriptable object GetDialogueGroupNames method

--- Assets/DialogueSystem/Scripts/ScriptableObjects/DSDialogueContainerSO.cs
+++ Assets/DialogueSystem/Scripts/ScriptableObjects/DSDialogueContainerSO.cs
@@ -16,5 +16,17 @@ namespace DS.ScriptableObjects
             DialogueGroups = new SerializableDictionary<DSDialogueGroupSO, List<DSDialogueSO>>();
             UngroupedDialogues = new List<DSDialogueSO>();
         }
+
+        public List<string> GetDialogueGroupNames()
+        {
+            List<string> dialogueGroupNames = new List<string>();
+
+            foreach (DSDialogueGroupSO dialogueGroup in DialogueGroups.Keys)
+            {
+                dialogueGroupNames.Add(dialogueGroup.GroupName);
+            }
+
+            return dialogueGroupNames;
+        }
     }
 }
