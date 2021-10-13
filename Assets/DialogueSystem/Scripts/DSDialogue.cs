ebaeaee070b2b3e6ea7a9fdc5927a1395992e362
Wafflus <wafflusdev@gmail.com>
2021-10-13 14:52:03 +0100

    Add dialogue and inspector variables to dialogue script

--- Assets/DialogueSystem/Scripts/DSDialogue.cs
+++ Assets/DialogueSystem/Scripts/DSDialogue.cs
@@ -2,7 +2,17 @@ using UnityEngine;
 
 namespace DS
 {
+    using ScriptableObjects;
+
     public class DSDialogue : MonoBehaviour
     {
+        /* Dialogue Scriptable Objects */
+        [SerializeField] private DSDialogueContainerSO dialogueContainer;
+        [SerializeField] private DSDialogueGroupSO dialogueGroup;
+        [SerializeField] private DSDialogueSO dialogue;
+
+        /* Filters */
+        [SerializeField] private bool groupedDialogues;
+        [SerializeField] private bool startingDialoguesOnly;
     }
 }
