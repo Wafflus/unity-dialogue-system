789b0fcccc67c9b3e5089ba6b418adee5172378d
Wafflus <wafflusdev@gmail.com>
2021-10-13 15:05:32 +0100

    Add serialized properties to inspector script

--- Assets/Editor/DialogueSystem/Inspectors/DSInspector.cs
+++ Assets/Editor/DialogueSystem/Inspectors/DSInspector.cs
@@ -5,6 +5,25 @@ namespace DS.Inspectors
     [CustomEditor(typeof(DSDialogue))]
     public class DSInspector : Editor
     {
+        /* Dialogue Scriptable Objects */
+        private SerializedProperty dialogueContainerProperty;
+        private SerializedProperty dialogueGroupProperty;
+        private SerializedProperty dialogueProperty;
+
+        /* Filters */
+        private SerializedProperty groupedDialoguesProperty;
+        private SerializedProperty startingDialoguesOnlyProperty;
+
+        private void OnEnable()
+        {
+            dialogueContainerProperty = serializedObject.FindProperty("dialogueContainer");
+            dialogueGroupProperty = serializedObject.FindProperty("dialogueGroup");
+            dialogueProperty = serializedObject.FindProperty("dialogue");
+
+            groupedDialoguesProperty = serializedObject.FindProperty("groupedDialogues");
+            startingDialoguesOnlyProperty = serializedObject.FindProperty("startingDialoguesOnly");
+        }
+
         public override void OnInspectorGUI()
         {
         }
